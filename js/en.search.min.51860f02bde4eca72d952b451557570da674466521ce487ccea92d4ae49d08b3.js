6bedd22c7f013cc828052195ac432b40c282da73
alexmisk <alexmisk@ya.ru>
2021-01-18 12:15:49 +0000

    Deploying to gh-pages from  @ 97003266965954d22e92401bafa30e2c676f87ee 🚀

--- js/en.search.min.51860f02bde4eca72d952b451557570da674466521ce487ccea92d4ae49d08b3.js
+++ js/en.search.min.51860f02bde4eca72d952b451557570da674466521ce487ccea92d4ae49d08b3.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch-ad47a5e1ee.min.js');loadScript('/js/en.search-data.min.e2fdee8310185dd7f9a3ab43954dbc60e1b7ccf941efd8859e8e8e94c4c8304e.js',function(){input.required=false;search();});}
+'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch-ad47a5e1ee.min.js');loadScript('/js/en.search-data.min.2292ee9b73cf9a8fa3836d4aba670f52c84fd95b171c6cfe47a8ec31e68e7dac.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){console.log("empty")
 results.classList.remove("has-hits");return;}
